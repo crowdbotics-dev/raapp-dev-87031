85e70bf55bd53f8eb3c04a49753bfbd939341cc8
Crowdbotics <engineering@crowdbotics.com>
2023-05-29 15:12:48 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/raapp_dev_87031/MainActivity.java
@@ -0,0 +1,15 @@
+package com.raapp_dev_87031;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "raapp_dev_87031";
+  }
+}
---
+++ backend/raapp_dev_87031/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for raapp_dev_87031 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'raapp_dev_87031.settings')
+
+application = get_wsgi_application()
